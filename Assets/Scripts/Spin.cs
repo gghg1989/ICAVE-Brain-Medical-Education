c49a8148281650c8e6cf354b35aa4f545127ed2d
gghg1989 <gghg1989@gmail.com>
2019-02-12 15:20:52 -0500

    Upload project

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+    public float speed = 0;
+
+	// Use this for initialization
+	void Start () {
+        MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
+        Mesh mesh = meshFilter.sharedMesh;
+        Color[] vertexColors = mesh.colors;
+        for (int i = 0; i < (int)(vertexColors.Length / 2); ++i)
+        {
+            vertexColors[i] = Color.blue;
+        }
+        mesh.colors = vertexColors; // update the vertex color
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+	}
+}
