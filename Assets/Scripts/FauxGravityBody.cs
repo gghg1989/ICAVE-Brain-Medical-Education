3af92553ae606fbec1865b757cf9279fa83ad5e0
gghg1989 <gghg1989@gmail.com>
2019-02-26 15:30:38 -0500

    Add faux gravity

---
+++ Assets/Scripts/FauxGravityBody.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityBody : MonoBehaviour {
+    public FauxGravityAttractor attractor;
+    private Transform myTransform;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        GetComponent<Rigidbody>().useGravity = false;
+        myTransform = transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        attractor.Attract(myTransform);
+	}
+}
